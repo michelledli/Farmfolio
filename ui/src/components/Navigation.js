a3561d4395ce28a77f9a61f5f3be040b6d04a4ed
sleepy-sun <99382676+sleepy-sun@users.noreply.github.com>
2023-04-03 14:01:23 -0700

    added components folder that contains layout elements

---
+++ ui/src/components/Navigation.js
@@ -0,0 +1,18 @@
+const Navigation = () => {
+    return (
+      <div>
+        <nav>
+          <ul>
+            <li><a class="dash-link" href="livestock.html">Livestock</a></li>
+            <li><a class="dash-link" href="news.html">News</a></li>
+            <li><a class="dash-link" href="addprofiles.html">Add Profile</a></li>
+            <li><a class="dash-link" href="login.html">Log out</a></li>
+          </ul>
+          <h1>Farmfolio</h1>
+          <input type="text" placeholder="Search.." />
+        </nav>
+      </div>
+    )
+  }
+  
+  export default Navigation
