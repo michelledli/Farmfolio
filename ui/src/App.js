6177d0636cc2b049f03c802203436fd6b09a2dfb
sleepy-sun <99382676+sleepy-sun@users.noreply.github.com>
2023-04-03 13:57:42 -0700

    added navigation and footer components

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,8 +1,10 @@
 import logo from './logo.svg';
+import Layout from './components/Layout';
 import './App.css';
 
 function App() {
   return (
+    <Layout>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,6 +21,7 @@ function App() {
         </a>
       </header>
     </div>
+    </Layout>
   );
 }
 
