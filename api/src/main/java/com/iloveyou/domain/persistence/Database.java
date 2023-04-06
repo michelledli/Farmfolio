e879eb2d7f8ec5219d37e55a3077cc1819769043
Ethan Werner <ewerner@csus.edu>
2023-04-05 22:24:18 -0700

    Move to proper package and fix H2 DB url

--- api/src/main/java/com/iloveyou/domain/persistence/Database.java
+++ api/src/main/java/com/iloveyou/domain/persistence/Database.java
@@ -1,3 +1,5 @@
+package com.iloveyou.domain.persistence;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -10,25 +12,29 @@ import java.util.Properties;
  */
 public class Database {
     static final String JDBC_DRIVER = "org.h2.Driver";
-    static final String DB_URL = "jdbc:h2:mem:farmfolio";
+    static final String DB_URL = "jdbc:h2:file:./resources/db";
     private Connection connection = null; 
     private Statement statement = null; 
     
-    private String createSchema = "CREATE SCHEMA farmfolio";
-    private String createEntity = "CREATE TABLE ENTITY" + 
+    public static String CREATE_SCHEMA = "CREATE SCHEMA farmfolio";
+    
+    public static String CREATE_ = "CREATE TABLE ENTITY" + 
                                 "(ID INT NOT NULL," +
                                 "PRIMARY KEY (`ID`))";
-    private String createFeature = "CREATE TABLE FEATURE" +  
+    
+    public static String createFeature = "CREATE TABLE FEATURE" +  
                                 "(ID INT NOT NULL AUTO_INCREMENT," + 
                                 "FEATURE_KEY VARCHAR(45) NOT NULL," + 
                                 "PRIMARY KEY (ID)," + 
                                 "UNIQUE INDEX FEATURE_KEY_UNIQUE (FEATURE_KEY ASC) VISIBLE," + 
                                 "UNIQUE INDEX FEATURE_LABEL_UNIQUE (FEATURE_LABEL ASC) VISIBLE)";
-    private String createFeature_Type = "CREATE TABLE FEATURE_TYPE" + 
+
+    public static String createFeature_Type = "CREATE TABLE FEATURE_TYPE" + 
                                         "(ID INT NOT NULL AUTO_INCREMENT," + 
                                         "TYPE VARCHAR(45) NOT NULL," + 
                                         "PRIMARY KEY (ID))";
-    private String createEntity_Feature = "CREATE TABLE ENTITY_FEATURE" +
+
+    public static String createEntity_Feature = "CREATE TABLE ENTITY_FEATURE" +
                                         "ENTITY_ID INT NOT NULL," +
                                         "FEATURE_ID VARCHAR(45) NOT NULL," + 
                                         "FEATURE_TYPE_ID VARCHAR(45) NOT NULL" + 
@@ -37,13 +43,13 @@ public class Database {
                                         "FOREIGN KEY (ENTITY_ID) REFERENCES ENTITY(ID)" + 
                                         "FOREIGN KEY (FEATURE_ID) REFERENCES FEATURE(ID)" + 
                                         "FOREIGN KEY (FEATURE_TYPE_ID) REFERENCES FEATURE_TYPE(ID))";
-    private String dropTablesSql;
+    public static String dropTablesSql = "";
 
     /**
      * Constructor which creates the Database object, wrapping the functionality of java.sql.Driver.connect()
      * @param connectionUrl
      */
-    Database(String connectionUrl) {
+    public Database(String connectionUrl) {
         try {
         Class.forName(JDBC_DRIVER);
         System.out.println("Connecting to database..."); 
@@ -58,7 +64,7 @@ public class Database {
     /**
      * Default constructor for the Database object. Creates a database instance using a default in memory database url.
      */
-    Database() {
+    public Database() {
         try {
             Class.forName(JDBC_DRIVER);
             System.out.println("Connecting to local database..."); 
@@ -74,7 +80,7 @@ public class Database {
      * Simple method to check if the Database has been connected to.
      * @return
      */
-    boolean isConnected() {
+    public boolean isConnected() {
         return (this.connection != null) ? true : false;
     }
 
@@ -82,14 +88,14 @@ public class Database {
      * Perform an arbitrary query upon the given database
      * @param query
      */
-    void performQuery(String query) throws SQLException {
+    public void performQuery(String query) throws SQLException {
         this.statement.executeQuery(query);
     }
 
     /**
      * Drop all tables from H2 and create the schema defined below
      */
-    void resetDatabase() {
+    public void resetDatabase() {
         // TODO
     }
 }
