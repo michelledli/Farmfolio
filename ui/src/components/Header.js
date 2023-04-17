781b491701b76c98f7c48872b35852cb3f1a33c2
sleepy-sun <99382676+sleepy-sun@users.noreply.github.com>
2023-04-17 00:17:02 -0700

    Added neccesary files for navigation

---
+++ ui/src/components/Header.js
@@ -0,0 +1,10 @@
+import { Navigation } from "./Navigation";
+const Header = () => {
+    return (
+      <>
+      <Navigation />
+    </>
+    )
+  }
+  
+  export default Header
---
+++ ui/src/pages/News.js
@@ -0,0 +1,9 @@
+export function News() {
+  return (
+    <div className='App'>
+    <header className='App-header'>
+    <h1>News</h1> 
+    </header>
+  </div>
+    );
+}
