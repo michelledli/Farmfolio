5518f0061192e8bda45b4db372990679e4a930d5
Dat Mai <datmai513@gmail.com>
2023-04-28 22:53:19 -0700

    ILY-51

---
+++ data-table/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import Table from './Table.js';
+import './table.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Table/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
