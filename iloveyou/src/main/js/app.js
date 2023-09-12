4865dfcc2fe1cc3118987246b94375ff97ec358a
Ethan Werner <>
2023-09-12 00:11:29 -0700

    Fix example component

--- iloveyou/src/main/js/app.js
+++ iloveyou/src/main/js/app.js
@@ -1,6 +1,5 @@
 const React = require("react");
 const ReactDOM = require("react-dom");
-import ExampleEntityList from "./example";
 import axios from "axios";
 
 class App extends React.Component {
@@ -20,6 +19,39 @@ class App extends React.Component {
 
   render() {
     return <ExampleEntityList exampleEntities={this.state.exampleEntities} />;
+  }
+}
+
+class ExampleEntityList extends React.Component {
+  render() {
+    const exampleEntities = this.props.exampleEntities.map((exampleEntity) => (
+      <ExampleEntity
+        key={exampleEntity._links.self.href}
+        exampleEntity={exampleEntity}
+      />
+    ));
+    return (
+      <table>
+        <tbody>
+          <tr>
+            <th>String</th>
+            <th>Number</th>
+          </tr>
+          {exampleEntities}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+class ExampleEntity extends React.Component {
+  render() {
+    return (
+      <tr>
+        <td>{this.props.exampleEntity.string}</td>
+        <td>{this.props.exampleEntity.number}</td>
+      </tr>
+    );
   }
 }
 
