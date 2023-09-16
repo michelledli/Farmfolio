71b65c862d384f68c21074ec168f2fe62080e2cf
sleepy-sun <99382676+sleepy-sun@users.noreply.github.com>
2023-09-16 14:35:14 -0700

    Added Family Tree placeholder page

---
+++ frontend/src/pages/Family Tree.js
@@ -0,0 +1,9 @@
+export function familyTree() {
+    return (
+      <div className='App'>
+      <header className='App-header'>
+      <h1>Family Tree</h1> 
+      </header>
+    </div>
+      );
+  }
