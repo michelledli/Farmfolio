d6666211b602c534463f2a4b504522f3b79cf016
sleepy-sun <99382676+sleepy-sun@users.noreply.github.com>
2023-09-16 15:44:55 -0700

    deleted old page

--- frontend/src/pages/Family Tree.js
+++
@@ -1,9 +0,0 @@
-export function familyTree() {
-    return (
-      <div className='App'>
-      <header className='App-header'>
-      <h1>Family Tree</h1> 
-      </header>
-    </div>
-      );
-  }
