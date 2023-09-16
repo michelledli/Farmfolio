02c24b2675a47ba514760613b2093bbecca080f6
Linh Dinh <linhvdinh1@gmail.com>
2023-09-16 14:55:40 -0700

    Beautified Home and Livestock page

---
+++ frontend/src/components/Cards.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import style from '../pages/Home.css'
+
+const Cards = ({image, name, age, gender}) => {
+  return (
+    <div>
+        <img src={image} className="App-logo" alt="logo" />
+        <div className='card'>Name: {name}</div>
+        <div className='card'>Age: {age}</div>
+        <div className='card'>Gender: {gender}</div>
+    </div>
+  )
+}
+
+export default Cards
