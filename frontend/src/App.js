0c7ee41e13b19eaff09f5962d657685d002ad3b1
Linh Dinh <linhvdinh1@gmail.com>
2023-09-16 14:54:30 -0700

    Beautified Home and Livestock page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import './Example.js';
 import Example from './Example.js';
+import { Home } from './pages/Home.js';
+import { Livestock } from './pages/Livestock';
 // import './api.js'
 // import Layout from './components/Layout';
 const React = require("react");
@@ -8,8 +10,8 @@ const React = require("react");
 class App extends React.Component {
 	render() {
 		return (
-      <Example></Example>
-		);
+			<Livestock />
+			);
 	}
 }
 
--- frontend/src/pages/Livestock.js
+++ frontend/src/pages/Livestock.js
@@ -1,9 +1,25 @@
+import "./Livestock.css";
+
 export function Livestock() {
   return (
-    <div className='App'>
-    <header className='App-header'>
-    <h1>Livestock</h1> 
-    </header>
-  </div>
-    );
-  }
+    <div className="App-header">
+        <h1>Livestock</h1>
+        <h2>Catalog</h2>
+        <button className="editButton">Edit</button>
+        <table className="catalog">
+          <thead>
+            <td>Name</td>
+            <td>Age</td>
+            <td>Gender</td>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Sawyer</td>
+              <td>2</td>
+              <td>Male</td>
+            </tr>
+          </tbody>
+        </table>
+    </div>
+  );
+}
