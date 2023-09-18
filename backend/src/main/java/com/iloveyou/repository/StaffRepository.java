778c177bf4dd6d9eeca896603457f275a7407e68
Niki <nikitakosyuk1@gmail.com>
2023-09-17 17:18:45 -0700

    added a StaffRepository interface to the project with default CRUD operations.

---
+++ backend/src/main/java/com/iloveyou/repository/StaffRepository.java
@@ -0,0 +1,15 @@
+package com.iloveyou.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.iloveyou.entity.StaffEntity;
+
+/**
+ * Staff repository interface that includes default CRUD operations.
+ */
+public interface StaffRepository extends CrudRepository<StaffEntity, Long> {
+    
+/**
+ * Custom User Repository operations can be added below.
+ */
+}
