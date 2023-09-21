fa02b8668067d93bf6cbc35d990813eb7ba80674
St0newall <jacksonmartin1977@gmail.com>
2023-09-20 20:21:06 -0700

    fixed animal controller completed add/delete/update requests

--- backend/src/main/java/com/iloveyou/controller/AnimalController.java
+++ backend/src/main/java/com/iloveyou/controller/AnimalController.java
@@ -2,12 +2,16 @@ package com.iloveyou.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,83 +19,44 @@ import org.springframework.web.bind.annotation.RestController;
 import com.iloveyou.entity.Animal;
 import com.iloveyou.repository.AnimalRepository;
 import java.util.Date;
-import java.util.List; 
+import java.util.List;
+import java.util.Optional; 
 
-
-@RequestMapping("animals")
+ 
+@RequestMapping("/animals")
 @RestController 
 public class AnimalController {
     @Autowired
-    AnimalRepository animalRepository; 
-    @GetMapping("/tag/{tag}")
-    @ResponseBody //finds by individual animal tag
-    public String getByTag(@PathVariable String tag) { 
-        List<Animal> animals = animalRepository.findByTag(tag); 
-        if(animals.size() == 0) {
-            return "No animal found with tag: " + tag;
-        }
-        return animals.get(0).toString();
-    } 
+    AnimalRepository animalRepository;  
+
 
     @GetMapping("/all")
-    @ResponseBody //finds all animals
-    public String getAll() { 
-        List<Animal> animals = (List<Animal>) animalRepository.findAll(); 
-        if(animals.size() == 0) {
-            return "No animals found";
-        }
-        String result = "";
-        for(Animal animal : animals) {
-            result += animal.toString() + "\n";
-        }
-        return result;
-    }
-    
-    @GetMapping("/create/{name}/{weight}/{tag}/{breed}")
-    @ResponseBody //creates animal
-    public String createAnimal(@PathVariable String name,@PathVariable int weight, @PathVariable String tag, @PathVariable String breed) { 
-        Animal animal = Animal.builder().name(name).weight(weight).tag(tag).breed(breed).build(); 
-        animalRepository.save(animal); 
-        return animal.toString();
-    } 
-
-    @GetMapping("/update/{name}/{weight}/{tag}/{breed}") //couldnt figure out DATE
-    @ResponseBody //updates and finds based off the tag of the animal
-    public String updateAnimal(@PathVariable String tag, @PathVariable String name,@PathVariable int weight, @PathVariable String breed) { 
-        List<Animal> animals = animalRepository.findByTag(tag); 
-        if(animals.size() == 0) {
-            return "No animal found with tag: " + tag;
-        }
-        Animal animal = animals.get(0); 
-        animal.setName(name); 
-        animal.setWeight(weight); 
-        animal.setBreed(breed); 
-        animalRepository.save(animal); 
-        return animal.toString();
-    } 
-
-    //updates tag 
-    @GetMapping("/update/{tag}/{newTag}")
-    @ResponseBody //updates and finds based off the tag of the animal
-    public String updateAnimalTag(@PathVariable String tag, @PathVariable String newTag) { 
-        List<Animal> animals = animalRepository.findByTag(tag); 
-        if(animals.size() == 0) {
-            return "No animal found with tag: " + tag;
-        }
-        Animal animal = animals.get(0); 
-        animal.setTag(newTag); 
-        animalRepository.save(animal); 
-        return animal.toString();
+    public List<Animal> getAllAnimals(){
+        return animalRepository.findAll();
     }
 
-    @GetMapping("/delete/{tag}")
-    @ResponseBody //deletes animal based off tag
-    public String deleteAnimal(@PathVariable String tag) { 
-        List<Animal> animals = animalRepository.findByTag(tag); 
-        if(animals.size() == 0) {
-            return "No animal found with tag: " + tag;
-        }
-        animalRepository.delete(animals.get(0)); 
-        return "Deleted animal with tag: " + tag;
+    @GetMapping("/{id}")
+    Optional<Animal> getAnimal(@PathVariable long id) {
+        return animalRepository.findById(id);
     } 
+
+    @PostMapping("/add")
+    public Animal createAnimal(@RequestBody Animal animal){
+        return animalRepository.save(animal); 
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteAnimal(@PathVariable("id") Long id){
+        animalRepository.deleteById(id);
+    } 
+
+    @PutMapping("/update/{id}") 
+    public ResponseEntity<Object> updateAnimal(@RequestBody Animal animal, @PathVariable Long id){
+        Optional<Animal> animalOptional = animalRepository.findById(animal.getId());
+        if(!animalOptional.isPresent())
+            return ResponseEntity.notFound().build();
+        animal.setId(id);
+        animalRepository.save(animal);
+        return ResponseEntity.noContent().build();
+    }
 }
--- backend/src/main/java/com/iloveyou/repository/AnimalRepository.java
+++ backend/src/main/java/com/iloveyou/repository/AnimalRepository.java
@@ -1,13 +1,13 @@
 package com.iloveyou.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import com.iloveyou.entity.Animal;
-import java.util.List; 
-import java.util.Date;
-
-public interface AnimalRepository extends CrudRepository<Animal, Long> { 
-    List<Animal> findByTag(String tag);
-    
+import java.util.List;
+import java.util.Optional; 
 
 
+public interface AnimalRepository extends JpaRepository<Animal, Long> { 
+    List<Animal> findAll();
+    //Animal findById(Long id);
 }
