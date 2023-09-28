8de17edcae6fed9c4a84b8975ac35b31eb712b9e
Ethan Werner <>
2023-09-27 19:40:29 -0700

    Fix another typo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,6 @@ import { Home } from './pages/Home.js';
 import Login from "./pages/Login.js";
 import Register from "./pages/Register.js";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-import Role from "./pages/Role";
 
 // import './api.js'
 import Layout from './components/Layout';
@@ -19,7 +18,6 @@ class App extends React.Component {
 		<Routes>
 			<Route path="/login" element={<Login />} />
 			<Route path="/register" element={<Register />} />
-			<Route path="/role" element={<Role />} />
 		</Routes>
 		);
 	}
