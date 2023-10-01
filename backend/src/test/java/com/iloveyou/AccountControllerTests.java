875f4792abe78421460aa5a2802886643a8e7939
Nikikosa <nikitakosyuk1@gmail.com>
2023-10-01 14:31:01 -0700

    Account controller tests written, deleted initial testing class.

--- backend/src/test/java/com/iloveyou/AccountControllerTests.java
+++ backend/src/test/java/com/iloveyou/AccountControllerTests.java
@@ -1,5 +1,6 @@
 package com.iloveyou;
 import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -12,9 +13,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
@@ -31,7 +35,7 @@ public class AccountControllerTests {
     
     private MockMvc mvc;
 
-    @Mock
+    @MockBean
     private AccountRepository accountRepo;
 
     @InjectMocks
@@ -45,8 +49,13 @@ public class AccountControllerTests {
 
     @Test
 	public void contextLoads() throws Exception {
-		Assert.isTrue(accController != null, "Something went wrong");
+		Assert.isTrue(accController != null, "Something went wrong with the Account Controller");
 	}
+
+    @Test
+    void shouldCreateMockMvc() {
+        assertNotNull(mvc, "MockMvc was not built");
+    }
 
     @Test
     public void CreateAccountTest() throws Exception {
@@ -62,19 +71,41 @@ public class AccountControllerTests {
                 "\"password\":\"Test222\"," +
                 "\"admin\":false}"
             )
- 
-        ).andExpect(status().isOk()).andReturn();
-        var temp2 = accController.getAccount(23);
-        MvcResult result2 = this.mvc.perform(get("/accounts")).andExpect(status().isOk()).andReturn();
-        String content = result2.getResponse().getContentAsString();
-        boolean temp = true;
+        ).andReturn();
+
+        Assert.isTrue(result.getResponse().getStatus() == 200, "POST request /add was not fulfilled. Status Code: " + result.getResponse().getStatus());
     }
 
     @Test
     public void getAllAccountsTest() throws Exception {
-        accController.createAccount(new Account((long)123, "Test", "Test", "Test@email.com", "Password", false));
-        MvcResult result = this.mvc.perform(get("/accounts/")).andExpect(status().isOk()).andReturn();
-        String content = result.getResponse().getContentAsString();
-        boolean temp = true;
+        MvcResult result = this.mvc.perform(get("/accounts")).andReturn();
+        Assert.isTrue(result.getResponse().getStatus() == 200, "GET request /accounts was not fulfilled. Status Code: " + result.getResponse().getStatus());
+        assertNotNull(result, "No content was returned");
+    }
+
+    @Test
+    //@Disabled // Disabled due to get not working for accounts/search endpoint
+    public void accountsearchTest() throws Exception {
+        MvcResult result = this.mvc.perform(get("/accounts/search").param("query", "test")).andReturn();
+        Assert.isTrue(result.getResponse().getStatus() == 200, "GET request /accounts was not fulfilled. Status Code: " + result.getResponse().getStatus());
+    }
+
+    @Test
+    public void getById() throws Exception {
+        MvcResult result = this.mvc.perform(get("/accounts/23")).andReturn();
+        Assert.isTrue(result.getResponse().getStatus() == 200, "GET request /accounts was not fulfilled. Status Code: " + result.getResponse().getStatus());
+    }
+
+    @Test
+    public void deleteById() throws Exception {
+        MvcResult result = this.mvc.perform(delete("/accounts/delete/23")).andReturn();
+        Assert.isTrue(result.getResponse().getStatus() == 200, "GET request /accounts was not fulfilled. Status Code: " + result.getResponse().getStatus());
+    }
+
+    @Test
+    @Disabled // post /accounts/update/{id} causes a 405 error "indicates that the server knows the request method, but the target resource doesn't support this method"
+    public void updateById() throws Exception {
+        MvcResult result = this.mvc.perform(post("/accounts/update/23")).andReturn();
+        Assert.isTrue(result.getResponse().getStatus() == 200, "GET request /accounts was not fulfilled. Status Code: " + result.getResponse().getStatus());
     }
 }
