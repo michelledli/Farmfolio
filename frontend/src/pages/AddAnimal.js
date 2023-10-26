113f0bf355d8213242d1465484f1e5f81df2caf8
Linh Dinh <linhvdinh1@gmail.com>
2023-10-25 20:33:12 -0700

    connected forum back and front end

--- frontend/src/pages/AddAnimal.js
+++ frontend/src/pages/AddAnimal.js
@@ -4,12 +4,12 @@ import axios from 'axios'
 const sendAddAnimal = (values) => {
   console.log(values)
   axios.post("api/animals/add", { 
-    id: values.ID, 
-    name: values.name,
-    dob: values.birthDate,
-    weight: values.weight,
-    tag: values.tag,
-    breed: values.breed,
+    id: values.target.ID, 
+    name: values.target.name,
+    dob: values.target.birthDate,
+    weight: values.target.weight,
+    tag: values.target.tag,
+    breed: values.target.breed,
   })
   .then(response => {
     console.log(response)
--- frontend/src/pages/News.js
+++ frontend/src/pages/News.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import PostList from "../components/PostList";
+import axios from "axios";
+import Announcements from "../components/Announcements";
 
-const News = ({ posts }) => {
+const News = () => {
+  const [posts, setPosts] = useState([]);
+  
+  useEffect(() => {
+    axios.get("api/posts")
+    .then(response => {
+      console.log(response.data)
+      setPosts(response.data)
+    }).catch(function (error) {
+      console.log(error)
+    });
+  },[] );
+
   console.log("Posts in ForumPage:", posts);
   return (
     <div className="App-header">
       <h1>Forum</h1>
+      <Announcements posts={posts}/>
       <PostList posts={posts} />
       <Link to="/create">
         <button>Create Post</button>
